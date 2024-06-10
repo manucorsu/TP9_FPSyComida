3630d5994fc205c4bab8c84f1df4990827a72a91
manucorsu <142500898+manucorsu@users.noreply.github.com>
2024-06-10 15:58:37 -0300

    agregar funcionalidad: Tarea

---
+++ Assets/InteractionAreaScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InteractionAreaScript : MonoBehaviour
+{
+    int puntaje;
+    public Text txtPuntaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        PickupScript pickup;
+        pickup = other.GetComponent<PickupScript>();
+        puntaje += pickup.puntosItem;
+        txtPuntaje.text = $"PUNTAJE: {puntaje}";
+        if(pickup.esComida == true)
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
